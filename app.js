305df63e19a0f2b60ee8131f19b2d455877a1ca3
Valeria Gabrielli <gabvale53@gmail.com>
2026-02-10 16:27:34 +0100

    feat: struttura app,.js

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 //importo express
+const express = require('express')
 //lo inizializzo invocandolo come funzione
+const app = express();
 //gli passo una porta a cui accedere
+const port = 3000; 
 //mi preparo già la comunicazione del router (gli dico ad app.js che esiste router.js)
 
 //definisco la prima rotta
-
+app.get('/' , (req, res) =>{
+    res.send("<h1>HOME</h1>")
+});
 
 
 //e metto il listen 
+app.listen(port, () =>{
+    console.log(`App in ascolto su porta ${port}`);
+});
